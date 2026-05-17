8ccc5e7571592a69c5e24fe43fd11037bffd9005
keisuke <k0629ike@gmail.com>
2026-05-17 23:11:32 +0900

    zisyo2

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-zisyo = {apple:ringo,appo:uncho}
-a = input("英単語を入力してください: ")
-print(english_words[str(a)])
+dict = {}
+while 2 > 1:
+    a = input("登録する概念の英字は？")
+    dict[a] = input(a + "の意味は？")
+    b = input("登録を続ける？(yes/no)") 
+    if b != "yes":
+        break
+c = input("意味を知りたい概念の英字は？")
+print(dict[c])
