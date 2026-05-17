9e4a6349ab4d0ec31aebdaa99f3bc644a82fe391
keisuke <k0629ike@gmail.com>
2026-05-18 00:01:09 +0900

    zisyosaisyuu

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ while 2 > 1:
     b = input("登録を続ける？(yes/no)") 
     if b != "yes":
         break
-print("登録された概念は" + itiran)
-c = input("意味を知りたい概念の英字は？")
-print(dict[c])
+print("登録された概念は" + str(itiran))
+d = input("知りたい概念の数は？")
+x = 0
+while x < int(d):
+    c = input("意味を知りたい概念の英字は？")
+    print(dict[c])
+    x += 1
