c9dd09e5aad2deb655042a6d5278ae193193f46b
keisuke <k0629ike@gmail.com>
2026-05-17 15:52:05 +0900

    tesuto while

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from programingupythonstudykupac!")
-
-
-if __name__ == "__main__":
-    main()
+def fizz_buzz(n):
+    if n % 15 == 0:
+        a ="FizzBuzz"
+    elif n % 3 == 0:
+        a = "Fizz"
+    elif n % 5 == 0:
+        a = "Buzz"
+    else :
+        a = str (n)
+    return a
+n=int(input ("判定する整数は？"))
