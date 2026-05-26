4356ba7d4cc84361e84bb456a5bfc01f5afa01e8
keisuke <k0629ike@gmail.com>
2026-05-26 20:46:57 +0900

    iikanzi

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-dict = {}
-itiran = []
-while 2 > 1:
-    a = input("登録する概念の英字は？")
-    dict[a] = input(a + "の意味は？")
-    itiran.insert(0, a)
-    b = input("登録を続ける？(yes/no)") 
-    if b != "yes":
-        break
-print("登録された概念は" + str(itiran))
-d = input("知りたい概念の数は？")
-x = 0
-while x < int(d):
-    c = input("意味を知りたい概念の英字は？")
-    print(dict[c])
-    x += 1
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
