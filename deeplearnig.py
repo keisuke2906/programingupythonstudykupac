3f9c562915f873d42bb5b521e01bc4bcec43ced0
keisuke <k0629ike@gmail.com>
2026-05-30 12:55:05 +0900

    machin

--- deeplearnig.py
+++ deeplearnig.py
@@ -9,12 +9,13 @@ def machine (x1, x2):
     w2  = np.array ([[7, 8], [9, 10],[11, 12]])
     b2  = 10
     a2  = np.dot (z1, w2) - b2
-    z2  = np.maximam(0, a2)
-    w3  = np.array ([13, 14], [15, 16])
+    z2  = np.maximum(0, a2)
+    w3  = np.array ([[13, 14], [15, 16]])
     b3  = 20
     a3  = np.dot (z2, w3) - b3
-    z2  = np.maximam(0, a3)
+    z2  = np.maximum(0, a3)
     return (z2)
-x1 = input("x1 = ?")
-x2 = input("x2 = ?")
-print(machine(x1, x2))
+x1 = float(input("x1 = ?"))
+x2 = float(input("x2 = ?"))
+y  = machine(x1, x2)
+print(y)
