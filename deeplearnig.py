33a5b6fdf729c7403849411ec9b81f084d7a4ac9
keisuke <k0629ike@gmail.com>
2026-05-30 12:43:27 +0900

    machin

--- deeplearnig.py
+++ deeplearnig.py
@@ -1,5 +1,20 @@
-import numpy as no
+import numpy as np
 import matplotlib.pyplot as pypy
-x = no.arange(0, 100, 0.001)
-y = no.sin(x)
-pypy.plot(x, y)
+def machine (x1, x2):
+    nyu = np.array ([x1, x2])
+    w1  = np.array ([[1, 2, 3], [4, 5, 6]])
+    b1  = 5
+    a1  = np.dot (nyu, w1) - b1
+    z1  = np.maximum(0, a1)
+    w2  = np.array ([[7, 8], [9, 10],[11, 12]])
+    b2  = 10
+    a2  = np.dot (z1, w2) - b2
+    z2  = np.maximam(0, a2)
+    w3  = np.array ([13, 14], [15, 16])
+    b3  = 20
+    a3  = np.dot (z2, w3) - b3
+    z2  = np.maximam(0, a3)
+    return (z2)
+x1 = input("x1 = ?")
+x2 = input("x2 = ?")
+print(machine(x1, x2))
