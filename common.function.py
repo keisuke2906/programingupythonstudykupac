d9101ddeac291dfe927b32696663d58a81ddbe89
keisuke <k0629ike@gmail.com>
2026-05-31 02:00:04 +0900

    function

---
+++ common.function.py
@@ -0,0 +1,14 @@
+def softmax(x):   #指数関数的で人間的な確率のとらえ方
+    import numpy as np
+    c = np.max(x)
+    exp_a = np.exp(x - c)
+    exp_sum = np.sum(exp_a)
+    y = exp_a / exp_sum
+    print (exp_a)
+    return y
+
+
+def sigmoid(x):
+    import numpy as np
+    y = 1 / (1 + np.exp(- x))
+    return y
--- softmax.py
+++
@@ -1,8 +0,0 @@
-def softmax(x):   #指数関数的で人間的な確率のとらえ方
-    import numpy as np
-    c = np.max(x)
-    exp_a = np.exp(x - c)
-    exp_sum = np.sum(exp_a)
-    y = exp_a / exp_sum
-    print (exp_a)
-    return (y)
