ff1332cec695624eb99c9539e1d46e905a975339
keisuke <k0629ike@gmail.com>
2026-06-01 20:47:32 +0900

    ｈｊ

--- omomi.py
+++ omomi.py
@@ -7,9 +7,9 @@ class omomi:
         self.W = np.random.rand(2 ,3)   #こいつがとりあえずの重みとなるのか？
     
     def predict (self, x):
-        return np.dot(x, self.W)
+        return np.dot(x, self.W)       #名前.predict(x)で簡単に予測　　predictは次の層にデータを渡す行為
 
     def loss(self, x, t):
-        z = self.predict(x)
+        z = self.predict(x)           #名前.loss(x, t)で簡単に誤差
         y = softmax(z)
         return cross_entropy(y, t)
