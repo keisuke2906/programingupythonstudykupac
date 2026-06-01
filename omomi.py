100c869f451fd0a9667cbf78382ee5f64ca425d7
keisuke <k0629ike@gmail.com>
2026-06-01 20:43:52 +0900

    oi

--- omomi.py
+++ omomi.py
@@ -4,7 +4,12 @@ import numpy as np
 from ainofunction import softmax, cross_entropy
 class omomi:
     def __init__(self):  #これはクラスを指定された時点で発動する
-        self.W = np.random.rand(2 ,3)
-        return np.dot(x , self.W)
-    def predict (self , x , t):
-        return softmax(x, t)
+        self.W = np.random.rand(2 ,3)   #こいつがとりあえずの重みとなるのか？
+    
+    def predict (self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        return cross_entropy(y, t)
